2c07c4a47270dabc93689db59f4d913a4a5bfd01
Raphael Rios <rios.raph@gmail.com>
2025-10-23 22:28:13 -0300

    feat: initial commit - build configuration for stock microservice

---
+++ src/main/java/br/com/dio/warehouse/entity/ProductEntity.java
@@ -0,0 +1,4 @@
+package br.com.dio.warehouse.entity;
+
+public class ProductEntity {
+}
---
+++ src/test/java/br/com/dio/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
